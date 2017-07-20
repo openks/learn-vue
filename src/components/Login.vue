cacc288c413069c5b4f4047faf701e97910de684
openks <zhuyangxing@foxmail.com>
2017-07-20 10:13:47 +0800

    1.添加登陆页面2.添加elementUI

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <el-input v-model="username" placeholder="请输入用户名"></el-input>
+    <el-input v-model="password" placeholder="请输入密码" type="password"></el-input>
+    <el-button type="button">登陆</el-button>
+  </div>
+</template>
+<script type="text/javascript">
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
 import Test from '@/components/Test';
+import Login from '@/components/Login';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/test',
       name: 'Test',
       component: Test,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
     },
   ],
 });
