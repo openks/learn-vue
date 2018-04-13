563a5c9fa4aa292b95108a17af62ccf80b5c5e2e
openks <zhuyangxing@foxmail.com>
2018-04-13 10:18:17 +0800

    修改文档正确连接并删除无关文件

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,21 +3,8 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="/#/test" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-      <li><router-link to="/test">Go to Foo</router-link></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-      <li><a href="/styleguide/" target="_blank">doc</a></li>
+      <li><a href="./styleguide/" target="_blank">文档</a></li>
+      <li><router-link to="/demo">demo</router-link></li>
     </ul>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
-import Test from '@/components/Test';
 import Login from '@/components/Login';
 import Demo from './demo.router';
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
-    },
-    {
-      path: '/test',
-      name: 'Test',
-      component: Test,
     },
     {
       path: '/login',
